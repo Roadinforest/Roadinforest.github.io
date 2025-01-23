6250ec8e7cbafbb92aa67a6a2947a4e4f12bce34
Roadinforest <whuforest@outlook.com>
2025-01-24 00:20:05 +0800

    Site updated: 2025-01-24 00:20:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/17/Docker-关于监听端口/","2025/01/23/test/","2024/09/14/CS61C/Lecture 3 Abstractions 1 Threads and Processes A quick, programmer’s viewpoint/","2024/11/04/字节内部课程/前端设计模式应用/","2024/11/04/字节内部课程/客户端容器/","2024/11/03/字节内部课程/计算机网络概论-字节内部课/"];function toRandomPost(){
+var posts=["2024/08/17/Docker-关于监听端口/","2024/11/04/字节内部课程/前端设计模式应用/","2024/11/03/字节内部课程/计算机网络概论-字节内部课/","2024/11/04/字节内部课程/客户端容器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
